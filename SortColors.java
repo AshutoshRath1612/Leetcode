5b8ec3704ee4332dd199777acf13e0ea13d36856
Ashutosh Rath <ashutoshrath1612@gmail.com>
2022-08-17 21:16:00 +0530

    Update SortColors.java

--- SortColors.java
+++ SortColors.java
@@ -2,6 +2,14 @@ package Leetcode;
 
 import java.util.Arrays;
 
+/*
+Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent,
+with the colors in the order red, white, and blue.
+
+We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+
+You must solve this problem without using the library's sort function.
+ */
 public class SortColors {
     public static void sortColors(int[] nums) {
         int i=0;
