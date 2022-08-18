9fd814e7f39c013306f8b89b889b0668b9dd6f04
Ashutosh Rath <ashutoshrath1612@gmail.com>
2022-08-18 17:16:05 +0530

    Update Smallest_letter_greater_then_target.java

--- Smallest_letter_greater_then_target.java
+++ Smallest_letter_greater_then_target.java
@@ -1,5 +1,12 @@
 package Leetcode;
 
+/*
+Given a characters array letters that is sorted in non-decreasing order and a character target, return the smallest character in the array that is larger than target.
+
+Note that the letters wrap around.
+
+For example, if target == 'z' and letters == ['a', 'b'], the answer is 'a'.
+ */
 public class Smallest_letter_greater_then_target {
     public static char nextGreatestLetter(char[] letters, char target) {
         int start = 0;
