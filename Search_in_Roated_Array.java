036da8e608e83e3836fac5477abe735603505820
Ashutosh Rath <ashutoshrath1612@gmail.com>
2022-08-19 21:31:37 +0530

    Update Search_in_Roated_Array.java

--- Search_in_Roated_Array.java
+++ Search_in_Roated_Array.java
@@ -35,6 +35,28 @@ public class Search_in_Roated_Array {
         }
         return -1;
     }
+    public static int peakDuplicate(int[]arr,int start ,int end){
+        while(start<=end) {
+        int mid =start+(end-start)/2;
+            if (mid<end&&arr[mid] > arr[mid + 1])
+                return mid;
+             if (mid>start&&arr[mid] < arr[mid - 1])
+                return mid-1;
+             if(arr[mid]==arr[start] && arr[mid]==arr[end]){
+                 if (arr[start]>arr[start+1])
+                     return start;
+                 start++;
+                 if (arr[end]>arr[end-1])
+                     return end;
+                 end--;
+             }
+             else if (arr[mid]>arr[start] || (arr[start]==arr[mid] && arr[mid]>arr[end]))
+                 start = mid+1;
+             else
+                 end = mid-1;
+        }
+        return -1;
+    }
     public static int findIndex(int[]arr,int start,int end,int target){
         int mid = start+(end-start)/2;
         if (start<=end){
