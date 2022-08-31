d49c24faf715a04f752f128be739134ff8b03e8d
Ashutosh Rath <ashutoshrath1612@gmail.com>
2022-08-31 23:51:36 +0530

    Update Search_in_Roated_Array.java

--- Search_in_Roated_Array.java
+++ Search_in_Roated_Array.java
@@ -71,8 +71,8 @@ public class Search_in_Roated_Array {
         return -1;
     }
     public static void main(String[] args) {
-        int[]arr ={1,3,5};
-        int target =3;
+        int[]arr ={2,5,6,0,0,1,2};
+        int target =0;
         int index=search(arr,target);
         System.out.println(index);
     }
