fd526298bda60c0d53416575d3b577986ce234ba
Ashutosh Rath <ashutoshrath1612@gmail.com>
2022-11-01 21:26:24 +0530

    Create DeleteNodeLL.java

---
+++ DeleteNodeLL.java
@@ -0,0 +1,12 @@
+package Leetcode;
+
+public class DeleteNodeLL {
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
