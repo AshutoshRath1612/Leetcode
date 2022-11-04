9792c48f0784c121ef6a2fb5a19c77a8a9212645
Ashutosh Rath <ashutoshrath1612@gmail.com>
2022-11-05 01:33:02 +0530

    Create ReverseString.java

---
+++ ReverseString.java
@@ -0,0 +1,22 @@
+package Leetcode;
+
+import java.util.Arrays;
+
+public class ReverseString {
+    public static void reverseString(char[] s) {
+        int i=0;
+        int j=s.length-1;
+        while (i<j){
+            char temp = s[i];
+            s[i]=s[j];
+            s[j]=temp;
+            i++;
+            j--;
+        }
+        System.out.println(Arrays.toString(s));
+    }
+    public static void main(String[] args) {
+        char []arr= {'h','e','l','l','o'};
+        reverseString(arr);
+    }
+}
