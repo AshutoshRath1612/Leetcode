7f267f919c46fa1f1b0c2b0569fe2cefbe041789
Ashutosh Rath <ashutoshrath1612@gmail.com>
2022-12-26 13:23:32 +0530

    Create BestTimeTOBuySell.java

---
+++ BestTimeTOBuySell.java
@@ -0,0 +1,19 @@
+package Leetcode;
+
+public class BestTimeTOBuySell {
+    public static int maxProfit(int[] prices) {
+        int max=0,min=Integer.MAX_VALUE;
+        for (int i=0;i<prices.length;i++){
+            if (prices[i]<min)
+                min = prices[i];
+            else if(prices[i] - min > max)
+                max = prices[i] -min;
+        }
+        return max;
+    }
+    public static void main(String[] args) {
+        int []prices = {7,1,5,3,6,4};
+        int maxprofit = maxProfit(prices);
+        System.out.println(maxprofit);
+    }
+}
