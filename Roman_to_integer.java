f7ebe5645c4148774b1686c35f3378ab06160534
Ashutosh Rath <ashutoshrath1612@gmail.com>
2024-06-25 15:05:13 +0530

    25/06/2024

--- Roman_to_integer.java
+++ Roman_to_integer.java
@@ -1,5 +1,7 @@
 package Leetcode;
 
+import java.util.HashMap;
+
 /*
 Given a roman numeral, convert it to an integer.
  */
@@ -115,6 +117,26 @@ public class Roman_to_integer {
             }
         }
         return num;
+    }
+    public int romanToInt3(String s) {
+        HashMap<Character,Integer> hm = new HashMap<>();
+        hm.put('I' , 1);
+        hm.put('V' , 5);
+        hm.put('X' , 10);
+        hm.put('L' , 50);
+        hm.put('C' , 100);
+        hm.put('D' , 500);
+        hm.put('M' , 1000);
+        int ans = 0;
+        for(int i =0  ; i<s.length();i++){
+            if(i < s.length()-1 && hm.get(s.charAt(i)) < hm.get(s.charAt(i+1))){
+                ans -= hm.get(s.charAt(i));
+            }
+            else{
+                ans += hm.get(s.charAt(i));
+            }
+        }
+        return ans;
     }
     public static void main(String[] args) {
         String str="MCMXCIV";
