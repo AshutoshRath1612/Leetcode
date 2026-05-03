25d3d3368f907ee0081ba0e21eb1981bd9fdd1ea
Ashutosh Rath <ashutoshrath1612@gmail.com>
2026-05-03 22:08:33 +0530

    Q82_RemoveDuplicateFromSortedListII

--- Q82_RemoveDuplicateFromSortedListII.java
+++ Q82_RemoveDuplicateFromSortedListII.java
@@ -1,12 +1,12 @@
 package Leetcode;
 
 public class Q82_RemoveDuplicateFromSortedListII {
-    public static ListNode deleteDuplicates(ListNode head) {
-        ListNode dummy = new ListNode(-1);
+    public static Leetcode.ListNode deleteDuplicates(Leetcode.ListNode head) {
+        Leetcode.ListNode dummy = new Leetcode.ListNode(-1);
         dummy.next = head;
 
-        ListNode prev = dummy;
-        ListNode curr = dummy.next;
+        Leetcode.ListNode prev = dummy;
+        Leetcode.ListNode curr = dummy.next;
 
         while (curr != null) {
             if (curr.next != null && curr.val == curr.next.val) {
@@ -26,13 +26,13 @@ public class Q82_RemoveDuplicateFromSortedListII {
     }
 
     public static void main(String[] args) {
-        ListNode head = Q2326_SpiralMatrixIV.makeListNodes(new int[]{1, 2, 3, 3, 4, 4, 5});
-        ListNode result = deleteDuplicates(head);
+        Leetcode.ListNode head = Q2326_SpiralMatrixIV.makeListNodes(new int[]{1, 2, 3, 3, 4, 4, 5});
+        Leetcode.ListNode result = deleteDuplicates(head);
         printLinkedList(result);
     }
 
-    public static void printLinkedList(ListNode head) {
-        ListNode temp = head;
+    public static void printLinkedList(Leetcode.ListNode head) {
+        Leetcode.ListNode temp = head;
 
         while (temp != null) {
             System.out.print(temp.val + "->");
